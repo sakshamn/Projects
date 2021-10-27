9195e4f13905ad60720a10d5896c2bf246b15876
sakshamn <75069095+sakshamn@users.noreply.github.com>
2021-10-27 11:19:40 +0000

    Add files via upload

--- ATG/SCRAPING/job_search/job_search/spiders/jobs.py
+++ ATG/SCRAPING/job_search/job_search/spiders/jobs.py
@@ -23,27 +23,11 @@ class JobsSpider(scrapy.Spider):
         if response.status != 200:
             return None    # Couldn't crawl webpage #
 
-        #items = JobSearchItem()
-        #body = response.body
+        jobs = response.css("div.col-md-4")
+        for job in jobs:
+            category = job.css("h2.c-font-bold a::attr(title)").extract()
+            subcategory = job.css("ul.c-content-list-1.c-theme.c-separator-dot li a::attr(title)").extract()
+            print(category, subcategory)
 
-        #categories = response.css("h2.c-font-bold a::attr(title)").extract()
-        ## Replace the spaces in the class name by "." #
-        #subcategories = response.css("ul.c-content-list-1.c-theme.c-separator-dot li a::attr(title)").extract()
-        #print(categories, "\n", subcategories)
-
-        output = response.css("div.col-md-4").getall()
-        print(output)
-
-        #for category in categories:
-        #    print(category)
-            #links = ultrabooks.css("div.archive-text2 a::attr(href)").extract()
-            #images = ultrabooks.css("div.archive-image img::attr(src)").extract()
-            #print("Link:\n", links)
-            #print("Image:\n", images)
-
-            #for link in links:
-            #    yield scrapy.Request(link, callback=self.parse_link)
-
-            #items["images"] = images
-            #yield items # send images to item class - JobSearchItem #
+# class JobsSpider # 
 
