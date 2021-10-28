3a3ec869023758f92e1dd6dd8458f6b8607ee2dc
sakshamn <75069095+sakshamn@users.noreply.github.com>
2021-10-28 08:48:30 +0000

    Add files via upload

---
+++ ATG/SCRAPING/job_search/job_search/job_search/spiders/jobs.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+# Imports Begin #
+#import scrapy
+#import BaseClassSpider
+# Imports End #
+
+# Imports Begin #
+import scrapy, sys
+# Imports End #
+
+
+# Globals Begin #
+OUTPUT_FILE = open("output.log", "w")
+
+# Preserve the standard output to a variable original_stdout. #
+# Change the standard output to a file so that all prints are redirected. #
+original_stdout = sys.stdout
+sys.stdout = OUTPUT_FILE
+# Globals End #
+
+
+#class JobsSpider(BaseClassSpider):
+class JobsSpider(scrapy.Spider):
+    name = 'jobs'
+    #allowed_domains = [
+    #    'www.careerguide.com/career-options',
+    #    'www.linkedin.com/jobs/search?keywords=&location=New%20Delhi%2C%20Delhi%2C%20India&geoId=&trk=homepage-jobseeker_jobs-search-bar_search-submit&position=1&pageNum=0'
+    #]
+    start_urls = [
+        'https://www.careerguide.com/career-options/',
+        'https://www.linkedin.com/jobs/search?keywords=&location=New%20Delhi%2C%20Delhi%2C%20India&geoId=&trk=homepage-jobseeker_jobs-search-bar_search-submit&position=1&pageNum=0/'
+    ]
+
+    def parse(self, response):
+        if response.status != 200:
+            self.logger.error("Couldn't Crawl Website!")
+            return None    # Couldn't crawl webpage #
+
+        #super().parse(response)
+
+        jobs = response.css("div.col-md-4")
+        links = []
+        for job in jobs:
+            category = job.css("h2.c-font-bold a::attr(title)").extract()
+            subcategories = job.css("ul.c-content-list-1.c-theme.c-separator-dot li a::attr(title)").extract()
+            print(category, subcategories)
+
+            for subcategory in subcategories:
+                links += [f"https://www.linkedin.com/jobs/search?keywords={subcategory}&location=New%20Delhi%2C%20Delhi%2C%20India&geoId=105953070&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0"]
+
+        for link in links:
+            print("Parsing the link:", link)
+            yield scrapy.Request(link, callback=self.parse_link)
+
+
+    def parse_link(self, response):
+        if response.status != 200:
+            self.logger.error("Couldn't Crawl Website!")
+            return None    # Couldn't crawl webpage #
+
+        title = response.css("title::text").get()
+        body = response.body
+
+        #table = response.css("table").extract()
+
+        print(body)
+        #print(table)
+
+# class JobsSpider # 
+
+
