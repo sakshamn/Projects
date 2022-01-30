8ef81a39ec67db850cc8ff986a1aa740ca6af26b
sakshamn <75069095+sakshamn@users.noreply.github.com>
2022-01-30 19:31:46 +0000

    Delete ATG directory

--- ATG/DJANGO/create_accounts/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CreateAccountsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'create_accounts'
--- ATG/DJANGO/create_accounts/models.py
+++
@@ -1,6 +0,0 @@
-# Import Begin #
-from django.db import models
-# Import End #
-
-# Create your models here.
-
